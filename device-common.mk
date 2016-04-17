d376b979bf8359fd991ece81fbd8a8f341dab58a
Milos Ratkovic <milosr@gmail.com>
2016-04-17 12:47:58 +0200

    added f2fs support, performance optimizations

--- device-common.mk
+++ device-common.mk
@@ -111,6 +111,8 @@ PRODUCT_PACKAGES += \
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
+    fsck.f2fs \
+    mkfs.f2fs \
     e2fsck \
     setup_fs
 
--- full_grouper.mk
+++ full_grouper.mk
@@ -29,4 +29,5 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 PRODUCT_NAME := full_grouper
 PRODUCT_DEVICE := grouper
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on Grouper
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
